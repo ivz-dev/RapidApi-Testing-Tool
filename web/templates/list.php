921402212a657641ce23e9832ef47aaf908f4a41
ivz-dev <zakutynsky@yahoo.com>
2017-10-27 10:35:30 +0300

    mock changes

--- web/templates/list.php
+++ web/templates/list.php
@@ -20,7 +20,7 @@
         <div class="navbar navbar-default navbar-fixed-top">
             <div class="container">
                 <div class="navbar-header">
-                    <a href="../" class="navbar-brand">Rapid testing tool</a>
+                    <a href="../" class="navbar-brand">RapidAPI testing tool</a>
                     <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbar-main">
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
